39288d9401bbdcd516d99d1c708b3baec062b3ab
Tyler White <tyler.white@snowflake.com>
2024-01-08 11:32:32 -0500

    feat: add run script for CI

---
+++ run.py
@@ -0,0 +1,13 @@
+import nbformat
+from nbconvert.preprocessors import ExecutePreprocessor
+from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv(override=True)
+
+notebooks = sorted(Path(".").glob("*.ipynb"), key=lambda f: int(f.name.split("_")[0]))
+
+for notebook in notebooks:
+    with open(notebook) as f:
+        nb = nbformat.read(f, as_version=4)
+    ep = ExecutePreprocessor().preprocess(nb, {"metadata": {"path": "./"}})
